805b7ee5b7e2e9715f6e92cff57720658179ad7f
FaboBorgesLima <fabo.borges.lima@gmail.com>
2025-05-19 21:00:23 -0300

    fix: removing create from user http adapter

--- src/user/user.http.adapter.ts
+++ src/user/user.http.adapter.ts
@@ -1,10 +1,8 @@
-import { UserCreateProps } from './types/user-create-props';
 import { UserUpdateProps } from './types/user-update-props';
 import { User } from './user';
 
 export interface UserHttpAdapter {
   findOne(id: string, authorization: string): Promise<User | null>;
-  create(user: UserCreateProps): Promise<{ user: User; token: string }>;
   update(
     userId: string,
     userUpdateProps: UserUpdateProps,
