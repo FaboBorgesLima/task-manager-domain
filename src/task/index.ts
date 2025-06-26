7db133ae7a316bb610a5aec33a9f1eddceb6f119
FaboBorgesLima <fabo.borges.lima@gmail.com>
2025-06-26 18:52:49 -0300

    fix: fix export

--- src/task/index.ts
+++ src/task/index.ts
@@ -1,3 +1,4 @@
 export { Task } from './task';
 export { TaskRepositoryInterface } from './task.repository.interface';
+export { TaskService } from './task.service';
 export { TaskStatus } from './task-status.enum';
